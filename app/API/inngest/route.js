eb07c6934ffa76f324c76544d04035110abbb3d8
Haroon Rasheed <haronworkspace@gmail.com>
2025-06-30 21:03:05 +0500

    vercel issue resolved

--- app/API/inngest/route.js
+++ app/API/inngest/route.js
@@ -5,9 +5,9 @@ import {
   syncUserUpdate,
 } from "@/config/inngest";
 import { serve } from "inngest/next";
-// Import the Inngest client and functions for user synchronization
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
+
+// Traditional Pages Router API route
+export default serve({
   client: inngest,
   functions: [syncUserCreation, syncUserUpdate, syncUserDeletion],
 });
