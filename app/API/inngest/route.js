d948895169313828170d45d3bb9bf178a58f2c26
Haroon Rasheed <haronworkspace@gmail.com>
2025-06-30 19:20:37 +0500

    First Commit

--- app/API/inngest/route.js
+++ app/API/inngest/route.js
@@ -5,7 +5,7 @@ import {
   syncUserUpdate,
 } from "@/config/inngest";
 import { serve } from "inngest/next";
-
+// Import the Inngest client and functions for user synchronization
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
